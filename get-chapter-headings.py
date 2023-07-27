aad993af9b8c2f595fe1a1b42820318dc00b0012
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-07-27 18:26:27 +0000

    Update get-chapter-headings.py

--- get-chapter-headings.py
+++ get-chapter-headings.py
@@ -15,7 +15,13 @@ version = "NIV"
 # Main
 #
 print("Which book of the Bible would you like headings for?")
-book = input("Please type the full name of the book: ").capitalize().strip()
+book_list = input("Please type the full name of the book: ").capitalize().strip().split()
+
+if len(book_list) == 2:
+    book = book_list[0] + " " + book_list[1].capitalize()
+else:
+    book = book_list[0].capitalize()
+
 print("Which chapter of " + book + " would you like the headings for?")
 chapter = input("Enter a number such as 1: ").strip()
 url = "https://www.biblegateway.com/passage/?search=" + book + "%20" + chapter + "&version=" + version
